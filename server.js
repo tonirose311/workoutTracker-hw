af5a66394728f8adf1388f365ce9dddf174874c9
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-03 16:48:54 -0600

    added workout functions

--- server.js
+++ server.js
@@ -25,35 +25,6 @@ mongoose.connect(
     }
   );
 
-  const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const path = require("path");
-
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
-
 require("./routes/api-routes.js")(app);
 
 app.get("/", (req, res) => {
@@ -66,10 +37,6 @@ app.get("/exercise", (req, res) => {
 
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
 });
 
 app.listen(PORT, () => {
